fdcc26a6965ce6d35456bb872f908673e1583d16
Princewill <91944411+Princewill-o@users.noreply.github.com>
2025-03-21 23:47:24 +0000

    Update HTTPServer.java

--- HTTPServer.java
+++ HTTPServer.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class HTTPServer {
-    private static final int PORT = 8080; // Change to 18080 for coursework challenge
+    private static final int PORT = 18080; // Change to 18080 for coursework challenge
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
